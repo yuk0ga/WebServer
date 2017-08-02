2298e3366d33c33f0775df059d1353b39ed01f16
Yu Koga <ukoooga@gmail.com>
2017-08-02 17:51:19 +0900

    reads html

--- WebServer.java
+++ WebServer.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.print.attribute.standard.RequestingUserName;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,6 +8,8 @@ import java.net.Socket;
  */
 public class WebServer {
 
+    private static String rootPath = "/Users/koga/Desktop";
+
     public static void main(String[] args) {
         int port = Integer.parseInt(args[0]);
         try {
@@ -16,12 +17,45 @@ public class WebServer {
             System.out.println("Web Server Started at port: " + ss.getLocalPort());
             Socket s = ss.accept();
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF-8"));
-            String line;
-            while ((line = in.readLine()) != null){
-                System.out.println(line);
+            String line = in.readLine();
+            String requestPath;
+            requestPath = rootPath + line.split(" ")[1];
+            if (requestPath.endsWith("/")){
+                requestPath += "index.html";
             }
+//            while ((line = in.readLine()) != null && !line.isEmpty()){
+//                System.out.println(line);
+//            }
+            System.out.println(requestPath);
+            getFile(s,requestPath);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void getFile(Socket s, String filePath) throws IOException{
+        PrintStream out = new PrintStream(s.getOutputStream(), true);
+        File file = new File(filePath);
+
+        try{
+            FileInputStream fis = new FileInputStream(file);
+//            FileReader r = new FileReader(file);
+//            BufferedReader br = new BufferedReader(r);
+            int fileLength = (int) file.length();
+            byte buff[] = new byte[fileLength];
+//            String line;
+//            while ((line = br.readLine()) != null){
+                out.println("HTTP/1.1 200 OK");
+                out.println("Content-Type: text/html");
+                out.println("Content-Length: " + fileLength);
+                out.println();
+
+                fis.read(buff);
+                out.write(buff, 0, fileLength);
+                out.flush();
+//            }
+        }catch (FileNotFoundException e){
+            out.println("404 Not Found");
         }
     }
 }
